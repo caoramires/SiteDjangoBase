69decc1dca6342936e6a5d77c0206c768d4466bf
caoramires <112504913+caoramires@users.noreply.github.com>
2024-03-04 16:22:31 -0300

    Telas de apresentação completas

--- grc/urls.py
+++ grc/urls.py
@@ -1,6 +1,6 @@
 
-from django.urls import path
-from .views import Homepage, Dashboard, Dimensoes
+from django.urls import path, reverse_lazy
+from .views import Homepage, Dashboard, Dimensoes, Processos, Procedimentos, Editarprocedimento, Editarperfil
 from django.contrib.auth import views as auth_view
 
 app_name = 'grc'
@@ -11,5 +11,11 @@ urlpatterns = [
     path('login/', auth_view.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('dimensoes/', Dimensoes.as_view(), name='dimensoes'),
+    path('processos/', Processos.as_view(), name='processos'),
+    path('procedimentos/', Procedimentos.as_view(), name='procedimentos'),
+    path('editarprocedimento/', Editarprocedimento.as_view(), name='editarprocedimento'),
+    path('editarperfil/<int:pk>', Editarperfil.as_view(), name='editarperfil'),
+    path('mudarsenha/', auth_view.PasswordChangeView.as_view(template_name="editarperfil.html",
+                                                             success_url=reverse_lazy('grc:dashboard')), name='mudarsenha'),
 
 ]
--- grc/views.py
+++ grc/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, reverse
-from .models import Framework, Dimension, Processo
+from .models import Framework, Dimension, Processo, Procedimento
 from .forms import FormHomepage
 from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -25,3 +25,43 @@ class Dimensoes(LoginRequiredMixin, ListView):
     template_name = "dimensoes.html"
     model = Dimension
     model = Processo
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["dimensoes"] = Dimension.objects.all()
+        return context
+
+class Processos(LoginRequiredMixin, ListView):
+    template_name = "processos.html"
+    model = Processo
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["processos"] = Processo.objects.all()
+        return context
+
+class Procedimentos(LoginRequiredMixin, ListView):
+    template_name = "procedimentos.html"
+    model = Procedimento
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context["procedimentos"] = Procedimento.objects.all()
+        context["processos"] = Processo.objects.all().prefetch_related('procedimento')
+        return context
+
+class Editarprocedimento(LoginRequiredMixin, UpdateView):
+    template_name = 'editarprocedimento.html'
+    model = Procedimento
+    fields = ['atendido']
+
+    def get_success_url(self):
+        return reverse('grc:procedimentos')
+
+class Editarperfil(LoginRequiredMixin, UpdateView):
+    template_name = 'editarperfil.html'
+    model = User
+    fields = ['first_name', 'last_name', 'email']
+
+    def get_success_url(self):
+        return reverse('grc:dashboard')
